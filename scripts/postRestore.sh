10fcf2d819eea3bb2f1b698ae2f39634562f8290
David Alimi <davealimi@gmail.com>
2024-05-12 15:12:09 +0300

    Improved backup script

--- scripts/postRestore.sh
+++ scripts/postRestore.sh
@@ -1,2 +1,10 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
 #Restart the stack after files are restored
 docker-compose up -d;
+
+sleep 20s;
+
+#restore from dump
+gunzip < ./lastDump.sql.gz | docker-compose exec -T database /usr/bin/mysql --binary-mode --user=root --password=$ADMIN_PASSWORD
